fe1bcaeeaa6a038af5bf7e7fb6709703be647b8d
Subhash Vadaparthi <2300030695cseh@gmail.com>
2025-08-11 11:14:11 +0530

    se

--- src/main/java/com/klu/AppController.java
+++ src/main/java/com/klu/AppController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@CrossOrigin(origins = "*")
+@CrossOrigin
 public class AppController {
 	@Autowired
 	Service s;
